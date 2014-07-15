d9d487b76678451a2286bc15bdae74d873374fb5
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-07-15 13:59:23 +0100

    move monster

--- monsters.py
+++ monsters.py
@@ -1,34 +1,50 @@
 # Monsters game 
 
-#read in world map containing cities
 print 'Loading Map...'
 with open('map.txt') as f:
   
-     import time
-     #time.sleep(1)
      print 'Loading Cities...'
+     citylist = []
+     directionmap = []
      for line in f:     
-        # load line into city, N, S, E and W
-        num = len(line.split()) 
-        if (num == 5):
-            city, P1, P2, P3, P4 = line.split()
-           # print city
-        elif (num == 4):
-        # Store Border City
-        #city, N
-            city, P1, P2, P3 = line.split()
-            #print 'Border City', city
-        else: 
-            city, P1, P2 = line.split()
-            print 'corner city'
-#for n range 10000:
+         #print 'load data into city, N, S, E and W'
+         num = len(line.split()) 
+         if (num == 5):
+             P0, P1, P2, P3, P4 = line.split()
+             citylist.append([P0])
+             directionmap.append([P1, P2, P3, P4])
+         elif (num == 4):
+            P0, P1, P2, P3 = line.split()
+            citylist.append([P0])
+            directionmap.append([P1, P2, P3])
+            #print 'Border City'
+         else: 
+            P0, P1, P2 = line.split()
+            citylist.append([P0])
+            directionmap.append([P1, P2])
+           #print 'corner city'
 
-#create monsters
-     user_input = raw_input("Generate How many monsters?")
-     print 'You have generated', user_input, 'Monsters.'
+# print 'create monsters'
+     #user_input = raw_input("Generate How many monsters?")
+user_input = 20
+print 'You have generated', user_input, 'Monsters.'
     
-# Assign monsts starting location randomly
-     import random
-     for N in Xrange(user_input): 
-         print ' Placing monster in home'
+# Assign monsters starting location randomly
+import random
+monster_city = []
+for N in xrange(user_input):
+    #print 'Placing monsters',
+    monster_city = random.choice(citylist)
+    monster_city.append([monster_city])
+    print 'Monster spawned in', monster_city
+    #monster_city = N, 'in city', monster_starting_city
+    
+#print ' Moving monsters'
+    move = random.choice(['North', 'South', 'East', 'West'])
+    print 'Move to', move
+
+#print 'Commense monster battle'
+
+
+
     
